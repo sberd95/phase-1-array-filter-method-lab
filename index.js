40f149385ac6ce1ca9d468064e1b8a3e07354b95
Sergio Berdeja <sergioberdeja@gmail.com>
2022-03-28 10:44:15 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(function(name) {return name.toUpperCase() === string.toUpperCase()})
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(name) {
+        let x = string.length
+        return name.slice(0,x) === string
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function(element) {return element.name === string})
+}
